946c3bbd975ae5032cce017e0be1747611b20a08
Joe Spedale <jspedale@gmail.com>
2016-11-20 22:22:33 -0500

    Changed bot.js to use ENV for heroku

--- bot.js
+++ bot.js
@@ -1,7 +1,13 @@
 var Twit = require('twit');
-var config = require('./config');
+//var config = require('./config');
 
-var T = new Twit(config);
+var T = new Twit({
+  consumer_key:         process.env.consumer_key,
+  consumer_secret:      process.env.consumer_secret,
+  access_token:         process.env.access_token,
+  access_token_secret:  process.env.access_token_secret
+});
+
 var trumpTwitter = 25073877;
 
 var stream = T.stream('statuses/filter', { follow: trumpTwitter });
